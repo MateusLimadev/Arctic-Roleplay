fbbb940726b45b8881204421922ef022a1ea68e9
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-12 14:38:28 -0300

    update

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+const toggleMenu = () => {
+    const navigation = document.querySelector('.navigation');
+
+
+    const burgerMenu = document.querySelector(".menu-icon");
+    const src = burgerMenu.getAttribute('src');
+
+    const isBurger = src === 'imagens/assets/burger-menu.svg';
+
+    const iconName = isBurger ?
+        'imagens/assets/close.svg'
+        :
+        'imagens/assets/burger-menu.svg';
+
+
+    burgerMenu.setAttribute(
+        'src',
+        iconName
+    );
+
+    if (!isBurger) {
+        navigation.classList.add("navigation--mobile--fadeout");
+        setTimeout(() => {
+            navigation.classList.toggle(
+                'navigation--mobile'
+            );
+        }, 300)
+    } else {
+        navigation.classList.remove("navigation--mobile--fadeout");
+        navigation.classList.toggle(
+            'navigation--mobile'
+        );
+    }
+};
