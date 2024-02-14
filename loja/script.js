1000c824ff7e20b114900d56d09546d83cee3fa0
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-14 19:29:33 -0300

    update

--- loja/script.js
+++
@@ -1,4 +0,0 @@
-function buyPackage(quantity, price) {
-    // Aqui você pode adicionar a lógica para processar a compra, por exemplo, redirecionar para um gateway de pagamento.
-    alert(`Você comprou ${quantity} gemas por R$ ${price.toFixed(2)}`);
-}
