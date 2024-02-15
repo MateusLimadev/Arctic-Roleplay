e3d3426a1a91036296e36b6265e373128ecee675
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-14 22:06:13 -0300

    update

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var prevScrollpos = window.pageYOffset;
-
-    window.addEventListener('scroll', function () {
-        var currentScrollPos = window.pageYOffset;
-        var header = document.getElementById('navbar');
-
-        if (prevScrollpos > currentScrollPos) {
-            // Rolar para cima - tornar o header sólido
-            header.classList.remove('transparent-blur');
-            header.classList.add('solid');
-        } else {
-            // Rolar para baixo - tornar o header transparente
-            header.classList.remove('solid');
-            header.classList.add('transparent-blur');
-        }
-
-        prevScrollpos = currentScrollPos;
-    });
-});
 
 
 const btnmobile = document.getElementById('btn-mobile');
