9770e3bea3aacae68c577411b20a63a05c397e2d
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-14 21:54:11 -0300

    update

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
         prevScrollpos = currentScrollPos;
     });
 });
+
+
+const btnmobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault(); // Fix the typo here
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) { 
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnmobile.addEventListener('click', toggleMenu);
+btnmobile.addEventListener('touchstart', toggleMenu);
