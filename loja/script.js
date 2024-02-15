f6b8025b6cec089bab428f87b02171dbd933bcf3
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-14 21:11:58 -0300

    Update script.js

--- loja/script.js
+++ loja/script.js
@@ -1,17 +1,17 @@
 const btnmobile = document.getElementById('btn-mobile');
+
 function toggleMenu(event){
-    if (event.type === 'touchstart') event.preventDefalt;
+    if (event.type === 'touchstart') event.preventDefault(); // Fix the typo here
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded', active)
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
     if (active) { 
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else{
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
-
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
-
 }
 
 btnmobile.addEventListener('click', toggleMenu);
