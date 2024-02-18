abd32c51c42151567c720ef0ae32fd0632b6cfcf
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-18 16:15:40 -0300

    update

--- script.js
+++ script.js
@@ -31,3 +31,21 @@ window.addEventListener('scroll', function () {
         header.classList.remove('transparent');
     }
 });
+
+
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
+
+let contador = 0;
+
+function slider() {
+    contador++;
+    
+    if (contador > imagens.length - 1) {
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 1280}px)`;
+}
+
+setInterval(slider, 3000);
