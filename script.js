ae1e11f6046ce9b814ce516d0557331fadea0bf4
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-18 17:09:21 -0300

    update nav

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 const btnmobile = document.getElementById('btn-mobile');
 
-function toggleMenu(event){
-    if (event.type === 'touchstart') event.preventDefault(); // Fix the typo here
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active);
 
-    if (active) { 
+    if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
     } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
@@ -19,16 +19,15 @@ function toggleMenu(event){
 btnmobile.addEventListener('click', toggleMenu);
 btnmobile.addEventListener('touchstart', toggleMenu);
 
-
 window.addEventListener('scroll', function () {
     var header = document.getElementById('header');
     var scrollPosition = window.scrollY;
 
     // Adiciona ou remove a classe 'transparent' com base no scroll
     if (scrollPosition > 0) {
-        header.classList.add('transparent');
-    } else {
         header.classList.remove('transparent');
+    } else {
+        header.classList.add('transparent');
     }
 });
 
