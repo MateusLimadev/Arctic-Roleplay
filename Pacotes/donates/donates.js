af07479d506230bc428d24e9b14fbbd5ee7622bf
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-28 23:09:59 -0300

    update

---
+++ Pacotes/donates/donates.js
@@ -0,0 +1,10 @@
+var details = document.getElementById('details');
+
+function showDetails(){
+    if(details.style.display === 'none'){
+         
+        details.style.display = 'block';
+    }else{
+          details.style.display = 'none';
+    }
+}
